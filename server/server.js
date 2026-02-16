1f198ab15d7e2fcc8537565039f4372e8ef34865
Sujitha <sujitha.p2023cse@sece.ac.in>
2026-02-16 18:40:13 +0530

    cors updated

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ const app = express();
 
 // Configure CORS properly
 app.use(cors({
-  origin: ["http://localhost:5173", "http://localhost:5174", "http://localhost:3000"],
+  origin: ["http://localhost:5173", "http://localhost:5174", "http://localhost:3000", "https://expense-ai-alpha.vercel.app"],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
